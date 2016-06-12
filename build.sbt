e4905525dfeac3533663e2968319b3634606d7fd
Matthew Frost <mf@matthewfrost.com>
2016-06-12 11:43:45 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "pvpupdater"
+
+version := "1.0"
+
+enablePlugins(JavaAppPackaging)
---
+++ src/main/scala/com/pvpleaderboard/updater/Main.scala
@@ -0,0 +1,7 @@
+package com.pvpleaderboard.updater
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Updating PvPLeaderboard DB");
+  }
+}
