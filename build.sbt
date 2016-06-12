0203660c045879f00b2b07a11992421a0fb8da7d
Matthew Frost <mf@matthewfrost.com>
2016-06-12 14:44:57 -0400

    added logback

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "pvpupdater"
 version := "1.0"
 
 enablePlugins(JavaAppPackaging)
+
+libraryDependencies += "ch.qos.logback" %  "logback-classic" % "1.1.7"
--- src/main/scala/com/pvpleaderboard/updater/Main.scala
+++ src/main/scala/com/pvpleaderboard/updater/Main.scala
@@ -1,7 +1,13 @@
 package com.pvpleaderboard.updater
 
+import org.slf4j.{ Logger, LoggerFactory }
+
 object Main {
+  private def logger: Logger = LoggerFactory.getLogger(this.getClass)
+
   def main(args: Array[String]): Unit = {
-    println("Updating PvPLeaderboard DB");
+    logger.info("Updating PvPLeaderboard DB")
+    // TODO
+    logger.info("PvPLeaderboard DB update complete")
   }
 }
