3c2e3642ddf286e9a61dc981d040b1233cc2cb5b
Matthew Frost <mf@matthewfrost.com>
2016-06-25 14:54:15 -0400

    return response from GET request

--- src/main/scala/com/pvpleaderboard/updater/ApiHandler.scala
+++ src/main/scala/com/pvpleaderboard/updater/ApiHandler.scala
@@ -1,11 +1,19 @@
 package com.pvpleaderboard.updater
 
+import java.io.IOException
+
+import scala.io.Source
+
+import org.slf4j.{ Logger, LoggerFactory }
+
 /**
  * Send requests and receive responses to/from the Blizzard API
  */
 class ApiHandler {
   private val BASE_URI: String = "https://us.api.battle.net/wow/"
   private val API_KEY: String = sys.env("BATTLE_NET_API_KEY")
+
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   def get(path: String, params: String = ""): Option[String] = {
     val requiredParams: String = String.format("?locale=en_US&apikey=%s", API_KEY)
@@ -14,8 +22,15 @@ class ApiHandler {
     } else {
       String.format("%s&%s", requiredParams, params)
     }
-    val uri: String = BASE_URI + path + allParams
-    println(uri) // TODO DELME
-    return Option("TODO")
+
+    try {
+      val response: String = Source.fromURL(BASE_URI + path + allParams).mkString
+
+      return Option(response)
+    } catch {
+      case io: IOException => logger.error("GET failed: " + io.toString())
+    }
+
+    return Option.empty
   }
 }
